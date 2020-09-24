7ae5d62fd52f3168d1c9f6dffe27e580ef37b7e9
De Zaldua Samuel <dezalduasamuel@MBP-de-De.lan>
2020-09-24 12:39:31 +0200

    creating an interface

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
+const firstValue = document.querySelector('#first-number');
+const secondValue = document.querySelector('#second-number');
+const operand = document.querySelector('#operand');
+const resultButton = document.querySelector('.result-button');
+const resultDisplay = document.querySelector('.result-display');
 
-        let firstValue = +prompt('Enter first value');
-        let operand = prompt('Enter operator');
-        let secondValue = +prompt('Enter second value');
+resultButton.addEventListener('click', () => {
+    let result;
+    switch (operand.value) {
+        case ('+'):
+            result = +firstValue.value + +secondValue.value;
+            break;
+        case ('-'):
+            result = +firstValue.value - +secondValue.value;
+            break;
+        case ('*'):
+            result = +firstValue.value * +secondValue.value;
+            break;
+        case ('/'):
+            result = +firstValue.value / +secondValue.value;
+            break;
+        default:
+            throw new Error("Invalid operator");
+    }
+    resultDisplay.innerHTML = result;
+})
 
-        switch (operand) {
-            case ('+'):
-                console.log(firstValue + secondValue);
-                break;
-            case ('-'):
-                console.log(firstValue - secondValue);
-                break;
-            case ('*'):
-                console.log(firstValue * secondValue);
-                break;
-            case ('/'):
-                console.log(firstValue / secondValue);
-                break;
-            default:
-                throw new Error("Invalid operator");
-        }
